d3f105d663774fa117ce3b35c832b8533294ba56
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2012-10-30 15:55:03 +0100

    Use on-disk database for testing < 1.4.

--- test_settings.py
+++ test_settings.py
@@ -4,6 +4,12 @@ DATABASES = {
     }
 }
 
+# On Django 1.3, use on-disk database for testing as sharing the database
+# with test thread is not supported there
+import django
+if django.VERSION[0] == 1 and django.VERSION[1] == 3:
+    DATABASES['default']['TEST_NAME'] = 'database.sqlite'
+
 INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
