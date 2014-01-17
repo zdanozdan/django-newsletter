0e608fcaf15ff2edcce9c8e1c71a060463acc072
Tomasz Zdanowski <tomasz@mikran.pl>
2014-01-17 16:39:51 +0100

    new

---
+++ newsletter/management/commands/import_users.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import User
+#from optparse import make_option
+from newsletter.models import Subscription,Newsletter,Message,List
+
+class Command(BaseCommand):
+
+    def import_auth_users(self, *args, **options):
+        auth_list, created = List.objects.get_or_create(name="django_auth")
+        users = User.objects.all()
+        for u in users:
+            try:
+                s = Subscription.objects.get(user=u)
+                print "skipping, user alread loaded from auth: ",s
+            except Subscription.DoesNotExist:
+                print "adding new user from auth: ",u
+                subscriber = Subscription(user=u,list=auth_list,subscribed=True)
+                subscriber.save()
+
+    def handle(self, *args, **options):
+        return self.import_auth_users(*args,**options)
